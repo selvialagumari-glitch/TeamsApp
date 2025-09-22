39179542ffe3fa60b643f545bc29bceceebb5b30
Alagumariselvi <alagumari.selvi@nordicglobal.com>
2025-09-22 16:10:34 +0530

    url changes

--- TeamsApp.Server/Program.cs
+++ TeamsApp.Server/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://teamsapp-enss.onrender.com")
+        policy.WithOrigins("https://teamsapp-integration.onrender.com")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
